4f356a0d0f7678432187d306feab4b6cf3e71e41
David Scoffield <davidscoffield01@gmail.com>
2021-10-03 20:33:54 -0300

    Agregacion de clase Permission para SQLAlchemy

---
+++ app/models/permission.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+class Permission(db.Model):
+
+    __tablename__ = "permissions"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
+
+    def __repr__(self):
+        return '<Permission %r>' % self.name

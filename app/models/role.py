ef0e73d2dc6f168d3095ea49dee09d016f68069d
David Scoffield <davidscoffield01@gmail.com>
2021-10-05 23:07:38 -0300

    Agregacion de constructor del modelo Role, y foreignKey

--- app/models/role.py
+++ app/models/role.py
@@ -9,6 +9,15 @@ class Role(db.Model):
     __tablename__ = "roles"
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255), nullable=False)
+    permissions = db.relationship(
+        "Permission",
+        secondary="role_has_permissions",
+        lazy="subquery",
+        backref=db.backref("roles", lazy=True),
+    )
 
     def __repr__(self):
-        return '<Role %r>' % self.name
+        return "<Role %r>" % self.name
+
+    def __init__(self, name: str = None):
+        self.name = name

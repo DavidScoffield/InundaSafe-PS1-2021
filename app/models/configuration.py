9763abc356a970ce587d13ceb1732f833706a7f2
David Scoffield <davidscoffield01@gmail.com>
2021-10-05 23:03:05 -0300

    Agregacion de constructor del modelo Configuration, y de foreignKeys

---
+++ app/models/configuration.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+class Configuration(db.Model):
+
+    __tablename__ = "configuration"
+    id = db.Column(db.Integer, primary_key=True)
+    elements_quantity = db.Column(db.Integer, nullable=False, default=50)
+    order_by = db.Column(db.String(25), nullable=False, default="asc")
+    colors_id_public = db.Column(
+        db.Integer, db.ForeignKey("colors.id"), nullable=False, default="asc"
+    )
+    colors_public = db.relationship("Color")
+    colors_id_private = db.Column(
+        db.Integer, db.ForeignKey("colors.id"), nullable=False, default="asc"
+    )
+    colors_private = db.relationship("Color")
+
+    def __repr__(self):
+        return "<Configuration %r>" % self.name
+
+    def __init__(
+        self,
+        elements_quantity: int = None,
+        order_by: str = None,
+        colors_id_public: int = None,
+        colors_id_private: int = None,
+    ):
+        self.elements_quantity = elements_quantity
+        self.order_by = order_by
+        self.colors_id_public = colors_id_public
+        self.colors_id_private = colors_id_private

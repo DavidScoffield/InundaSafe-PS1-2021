ae45477cc4f60ede4c69f46fc8920a1211c48c63
David Scoffield <davidscoffield01@gmail.com>
2021-10-06 23:33:30 -0300

    Correcion de columna en role_has_permissions

--- app/models/role_has_permissions.py
+++ app/models/role_has_permissions.py
@@ -8,7 +8,7 @@ role_has_permissions = db.Table(
     "role_has_permissions",
     db.Column("role_id", db.ForeignKey("roles.id"), primary_key=True),
     db.Column(
-        "right_id",
+        "permission_id",
         db.ForeignKey("permissions.id"),
         primary_key=True,
     ),

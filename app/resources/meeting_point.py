22e5e2eca9bbc752cdd1a46f2c8acb8ca911c3c2
Valentin Giorgetti <valengamer13@gmail.com>
2021-10-10 13:08:09 -0300

    Al enviar los datos del formulario el controller invoca al método del modelo para crear y guardar el meeting point

--- app/resources/meeting_point.py
+++ app/resources/meeting_point.py
@@ -7,4 +7,8 @@ meeting_point = Blueprint("meeting_point", __name__, url_prefix="/meeting-point"
 @meeting_point.route("/new", methods = ['GET','POST'])
 def new():
 
+    if request.method == "POST":
+
+        MeetingPoint.meeting_point_new(**request.form)
+
     return render_template("meeting_point/new.html")

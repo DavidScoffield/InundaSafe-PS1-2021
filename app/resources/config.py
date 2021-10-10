c115fc3e7c7b50bdd07d1fb774c7d5d565b8eaea
David Scoffield <davidscoffield01@gmail.com>
2021-10-10 11:39:06 -0300

    Se agrega controller para actualizar la configuracion de la aplicacion

--- app/resources/config.py
+++ app/resources/config.py
@@ -1,6 +1,4 @@
-from flask import Blueprint
-from flask.templating import render_template
-
+from flask import Blueprint, redirect, request, url_for, render_template
 from app.models.colors import Color
 from app.models.configuration import Configuration
 
@@ -13,3 +11,17 @@ def index():
     config_actual = Configuration.actual()
 
     return render_template("config/index.html", colors=colors, config=config_actual)
+
+
+@config_routes.post("/", endpoint="update")
+def update():
+    data = request.form
+
+    Configuration.update(
+        order_by=data["order_by"],
+        elements_quantity=data["elements_quantity"],
+        colors_id_public=data["color_public"],
+        colors_id_private=data["color_private"],
+    )
+
+    return redirect(url_for("config_routes.index"))

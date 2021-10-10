d5ce786d8afa77921e20889f7bd00abd85f17493
David Scoffield <davidscoffield01@gmail.com>
2021-10-10 00:26:19 -0300

    El controller de config recupera datos necesarios para la vista

--- app/resources/config.py
+++ app/resources/config.py
@@ -1,9 +1,15 @@
 from flask import Blueprint
 from flask.templating import render_template
 
-config_routes = Blueprint("config_routes", __name__, url_prefix="/")
+from app.models.colors import Color
+from app.models.configuration import Configuration
+
+config_routes = Blueprint("config_routes", __name__, url_prefix="/config")
 
 
 @config_routes.get("/")
 def index():
-    return render_template("config/index.html")
+    colors = Color.all()
+    config_actual = Configuration.actual()
+
+    return render_template("config/index.html", colors=colors, config=config_actual)

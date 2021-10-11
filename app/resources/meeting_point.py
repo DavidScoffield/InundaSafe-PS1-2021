6d339c621140fcb1fd937455cab6001637943846
Valentin Giorgetti <valengamer13@gmail.com>
2021-10-11 15:57:10 -0300

    Importo las librerías y defino una clase para crear el formulario de alta de un punto de encuentro
    - Valida que el campo "Nombre" y "Dirección" no estén vacíos
    - Valida que el campo "Nombre" no tenga caracteres que no sean letras
    - Valida que las coordenadas y el teléfono no tengan caracteres que no sean números
    - Valida que el formato del email sea correcto

--- app/resources/meeting_point.py
+++ app/resources/meeting_point.py
@@ -1,9 +1,20 @@
 from app.models.meeting_point import MeetingPoint
 from flask import render_template, Blueprint, request, flash, redirect, url_for
 from app.db import db
-from app.helpers.validators import is_empty
+from wtforms import SubmitField, validators, IntegerField, StringField, SelectField
+from flask_wtf import Form
 
 meeting_point = Blueprint("meeting_point", __name__, url_prefix="/meeting-point")
+
+class NewMeetingPointForm(Form):
+    name = StringField('Nombre', [validators.DataRequired(message = "Este campo es obligatorio"), validators.Regexp('^[a-z A-Z]+$', message = "Por favor, ingrese un nombre válido")])
+    address = StringField('Dirección', [validators.DataRequired(message = "Este campo es obligatorio")])
+    coor_X = StringField('Coordenada X', [validators.Optional(), validators.Regexp('^[\d]+$', message = "Por favor, ingrese una coordenada X válida")])
+    coor_Y = StringField('Coordenada Y', [validators.Optional(), validators.Regexp('^[\d]+$', message = "Por favor, ingrese una coordenada Y válida")])
+    telephone = StringField('Teléfono', [validators.Optional(), validators.Regexp('^[\d]+$', message = "Por favor, ingrese un número de teléfono válido")])
+    email = StringField('Email', [validators.Email(message = "Por favor, ingrese un email válido"), validators.Optional()])
+    state = SelectField('Estado', choices = [("publicated", "Publicado"), ("despublicated", "Despublicado")])
+    submit = SubmitField('Guardar', render_kw={'class':'button-gradient'})
 
 @meeting_point.route("/new", methods = ['GET','POST'])
 def new():

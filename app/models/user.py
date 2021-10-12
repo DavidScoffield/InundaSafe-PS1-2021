85d610436f59dd92c49b108ce7aa99227195052e
David Scoffield <davidscoffield01@gmail.com>
2021-10-11 23:31:50 -0300

    Modificacion de password en modelo, y automatizacion para guardar la contraseña hasheada y otros metodos para realizar estas tareas

--- app/models/user.py
+++ app/models/user.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
-from app.db import db
-from app.models.user_has_roles import user_has_roles
 from sqlalchemy import or_
 import datetime
+from app.db import db
+from app.helpers.bcrypt import check_password, generate_password_hash
+from app.models.user_has_roles import user_has_roles
 
 
 class User(db.Model):
@@ -11,11 +12,13 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(150), nullable=False, unique=True)
     username = db.Column(db.String(50), nullable=False, unique=True)
-    password = db.Column(db.String(50), nullable=False)
+    password_hash = db.Column(db.String(), nullable=False)
     first_name = db.Column(db.String(50), nullable=False)
     last_name = db.Column(db.String(50), nullable=False)
     active = db.Column(db.Integer, nullable=False)
-    created_at = db.Column(db.DateTime, default=datetime.datetime.utcnow) #se modifico para que se guarde la fecha en la bd
+    created_at = db.Column(
+        db.DateTime, default=datetime.datetime.utcnow
+    )  # se modifico para que se guarde la fecha en la bd
     updated_at = db.Column(db.DateTime, default=datetime.datetime.utcnow)
     roles = db.relationship(
         "Role",
@@ -45,51 +48,42 @@ class User(db.Model):
 
     @classmethod
     def find_by_email_and_pass(cls, email, password):
-        return User.query.filter(User.email==email and User.password==password).first()
+        """
+        - Busca en la base de datos a un usuario que tenga el mismo mail
+        - Si lo encuentra cumprueba que el que recupera tenga la misma contraseña que la que llega como parametro, y devuelve al usuario encontrado
+        - Caso contrario retorna None
+        """
+
+        user_find = User.query.filter(User.email == email).first()
+        return (
+            user_find if (user_find and user_find.verify_password(password)) else None
+        )
 
     @classmethod
     def check_existing_email_or_username(cls, email, username):
-        return User.query.filter(or_(User.username==username, User.email==email)).first()
+        return User.query.filter(
+            or_(User.username == username, User.email == email)
+        ).first()
 
     @classmethod
     def insert_user(cls, new_user):
         db.session.add(new_user)
-        db.session.commit()   
+        db.session.commit()
 
     @classmethod
     def find_all_users(cls):
         return User.query.all()
 
-# class User(object):
-#     @classmethod
-#     def all(cls, conn):
-#         sql = "SELECT * FROM users"
-#         cursor = conn.cursor()
-#         cursor.execute(sql)
+    @property
+    def password(self):
+        "Creacion de excepcion en caso de que se quiera hacer el get de password"
+        raise AttributeError("Contraseña no legible")
 
-#         return cursor.fetchall()
+    @password.setter
+    def password(self, password):
+        "Setter para la password, y al momento de realizarla, crear la contraseña hasheada y almacenar esta en vez del string"
+        self.password_hash = generate_password_hash(password)
 
-#     @classmethod
-#     def create(cls, conn, data):
-#         sql = """
-#             INSERT INTO users (email, password, first_name, last_name)
-#             VALUES (%s, %s, %s, %s)
-#         """
-
-#         cursor = conn.cursor()
-#         cursor.execute(sql, list(data.values()))
-#         conn.commit()
-
-#         return True
-
-#     @classmethod
-#     def find_by_email_and_pass(cls, conn, email, password):
-#         sql = """
-#             SELECT * FROM users AS u
-#             WHERE u.email = %s AND u.password = %s
-#         """
-
-#         cursor = conn.cursor()
-#         cursor.execute(sql, (email, password))
-
-# return cursor.fetchone()
+    def verify_password(self, password: str):
+        "Comprueba que la contraseña pasada sea la misma que la que tiene almacenada hasheada"
+        return check_password(self.password_hash, password)

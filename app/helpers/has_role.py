6108652cad2444e68fea801737675d69b0dd490d
David Scoffield <davidscoffield01@gmail.com>
2021-10-13 23:31:29 -0300

    Se arregla docstring en la funcion has_roles

--- app/helpers/has_role.py
+++ app/helpers/has_role.py
@@ -1,5 +1,5 @@
 def has_role(roles, rol_to_check):
-    """Llega una lista de roles, busca si el name de alguno es == rol_administrador"""
+    """Llega una lista de roles, busca si el name de alguno es == rol_to_check"""
     for rol in roles:
         if rol.name == rol_to_check:
             return True

0ea90ad867122288a97d665780d038584c281cdd
David Scoffield <davidscoffield01@gmail.com>
2021-10-16 23:00:14 -0300

    Modificacion de pagina home

--- app/resources/home.py
+++ app/resources/home.py
@@ -1,5 +1,12 @@
-from flask import Blueprint, redirect, url_for, render_template, session
+from flask import (
+    Blueprint,
+    redirect,
+    url_for,
+    render_template,
+    session,
+)
 from app.helpers.auth import authenticated
+from app.models.user import User
 
 home = Blueprint("home", __name__, url_prefix="/")
 
@@ -10,4 +17,7 @@ def index():
     if not authenticated(session):
         return redirect(url_for("auth_routes.auth_login"))
 
-    return render_template("home.html")
+    user_id = authenticated(session)
+    user = User.find_user_by_id(user_id)
+
+    return render_template("home.html", user=user)

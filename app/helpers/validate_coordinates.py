7e5965bc62d7a361353a04d92542bda74a2d321b
Valentin Giorgetti <valengamer13@gmail.com>
2021-10-31 14:56:07 -0300

    Defino el formulario para obtener los datos de un recorrido de evacuación
    También se define un validador para coordenadas

---
+++ app/helpers/validate_coordinates.py
@@ -0,0 +1,12 @@
+import re
+
+def validate_coordinates(coordinates):
+    """"Verifica si el formato de la coordenada es válido.
+
+        'coordinates': lista que contiene la latitud y longitud"""
+
+    for coordinate in coordinates:
+        if not re.fullmatch("^-?[0-9]{1,100}(?:\.[0-9]+)?$", str(coordinate)):
+            return False
+    
+    return True

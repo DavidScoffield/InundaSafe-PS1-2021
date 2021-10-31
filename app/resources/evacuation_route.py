6606b36d19c705b4f9c951998789b326817d22a2
Valentin Giorgetti <valengamer13@gmail.com>
2021-10-31 14:51:30 -0300

    Defino los controladores para realizar el alta de un recorrido de evacuación
    También se define y registra en la aplicación el blueprint para el apartado de recorridos de evacuación

---
+++ app/resources/evacuation_route.py
@@ -0,0 +1,75 @@
+from app.models.evacuation_route import EvacuationRoute
+from app.helpers.forms.evacuation_route_form import (
+    EvacuationRouteForm
+)
+from flask import (
+    render_template,
+    Blueprint,
+    request,
+    flash,
+    redirect,
+    url_for,
+    session,
+    abort,
+)
+from app.db import db
+from app.helpers.auth import authenticated
+from app.helpers.check_permission import check_permission
+from app.helpers.check_param_search import (
+    check_param,
+)
+from app.helpers.validate_coordinates import validate_coordinates
+import json
+
+evacuation_route = Blueprint(
+    "evacuation_route", __name__, url_prefix="/evacuation-route"
+)
+
+
+@evacuation_route.get("/new")
+def new():
+    "Controller para mostrar el formulario para el alta de un recorrido de evacuación"
+
+    if not authenticated(session) or not check_permission(
+        "evacuation_route_new"
+    ):
+        abort(401)
+
+    form = EvacuationRouteForm()
+
+    return render_template("evacuation_route/new.html", form=form)
+
+
+@evacuation_route.post("/new")
+def create():
+    "Controller para crear el recorrido de evacuación a partir de los datos del formulario"
+    
+    if not authenticated(session) or not check_permission(
+        "evacuation_route_create"
+    ):
+        abort(401)
+
+    form = EvacuationRouteForm(request.form)
+    if not form.validate_on_submit():
+        flash("Por favor, corrija los errores",
+              category="evacuation_route_new")
+    else:
+        args = form.data
+        del args["submit"]
+        del args["csrf_token"]
+        del args["id"]
+        if EvacuationRoute.exists_name(args["name"]):
+            flash("Ya existe un recorrido de evacuación con ese nombre",
+                   category="evacuation_route_new")
+        else:
+            args["coordinates"] = json.loads(args["coordinates"])
+            for coordinate in args["coordinates"]:
+                if not validate_coordinates(coordinate):
+                    flash("Se ingresó una coordenada inválida", category="evacuation_route_new")
+                    return render_template("evacuation_route/new.html", form=form)
+
+            EvacuationRoute.new(**args)
+            flash("Recorrido de evacuación agregado exitosamente",
+                   category="evacuation_route_new")
+
+    return render_template("evacuation_route/new.html", form=form)

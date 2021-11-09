9b27b865664dbe754ccdd9ba9013c77760bb6b38
Marcon, Paula <paulamarcon1710@gmail.com>
2021-11-08 22:51:50 -0300

    hago el wtfform para crear un seguimiento, con su newhtml

---
+++ app/helpers/forms/follow_up_form.py
@@ -0,0 +1,32 @@
+from wtforms import (
+    SubmitField,
+    validators,
+    StringField,
+)
+from flask_wtf import FlaskForm
+
+
+class FollowUpForm(FlaskForm):
+
+    "Crea el formulario para obtener datos de un seguimiento"
+
+    description = StringField(
+        "Descripción (*)",
+        [
+            validators.DataRequired(
+                message="Este campo es obligatorio"
+            ),
+            validators.Regexp(
+                "^[a-zA-Z0-9 ]+$",
+                message="Por favor, ingrese una descripción válida. La descripción no puede tener caracteres especiales.",
+            ),
+        ],
+        render_kw={
+            "pattern": "^[a-zA-Z0-9 ]+$",
+            "title": "La descripción no puede tener caracteres especiales",
+        },
+    )
+
+    submit = SubmitField(
+        "Guardar", render_kw={"class": "button-gradient"}
+    )

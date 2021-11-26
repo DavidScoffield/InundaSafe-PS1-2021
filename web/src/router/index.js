f91390d8d0f907e9ea59cc5a217221bcf587c63f
Valentin Giorgetti <valengamer13@gmail.com>
2021-11-26 18:17:00 -0300

    Se utiliza vue-router
    Se declara la ruta para acceder al apartado de puntos de en encuentro y recorridos de evacuación

---
+++ web/src/router/index.js
@@ -0,0 +1,17 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import MeetingPointsEvacuationRoutes from '../components/MeetingPointsEvacuationRoutes.vue'
+
+const routes = [
+    {
+        path: '/puntos-recorrdidos',
+        name: 'MeetingPointsEvacuationRoutes',
+        component: MeetingPointsEvacuationRoutes
+    }
+]
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes,
+});
+
+export default router

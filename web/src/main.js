c88a7fb0c8bb13c4b8705ce2fb6c871c1e60c4fe
Valentin Giorgetti <valengamer13@gmail.com>
2021-11-26 19:08:38 -0300

    Se agrega bootstrap

--- web/src/main.js
+++ web/src/main.js
@@ -2,5 +2,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import "leaflet/dist/leaflet.css"
+import 'jquery'
+import '@popperjs/core'
+import 'bootstrap/dist/js/bootstrap.bundle'
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap"
 
 createApp(App).use(router).mount('#app')

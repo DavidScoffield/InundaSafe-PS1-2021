7a367d7632541a9534f5eb9e55889b369b0650fe
Marcon, Paula <paulamarcon1710@gmail.com>
2021-11-27 22:05:48 -0300

    nueva path de newComplaint se agrega en el index de router

--- web/src/router/index.js
+++ web/src/router/index.js
@@ -1,11 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Complaint from '../components/Complaint.vue'
+import FormNewComplaint from '../components/FormNewComplaint.vue'
 
 const routes = [
     {
         path: '/complaints',
         name: 'Complaints',
         component: Complaint
+    },
+    {
+        path: '/newComplaint',
+        name: 'NewComplaint',
+        component: FormNewComplaint
     }
 ]
 

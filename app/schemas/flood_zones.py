862af9541bf24eb205b7b883cddd9450ce18b681
David Scoffield <davidscoffield01@gmail.com>
2021-12-02 09:30:59 -0300

    Se agrega devolucion de mas campos en Schema FloodZones, y formateo a español del estado luego del dump

--- app/schemas/flood_zones.py
+++ app/schemas/flood_zones.py
@@ -1,20 +1,40 @@
-from marshmallow import Schema, fields
+from marshmallow import Schema, fields, post_dump
 from app.schemas.coordinate import coordinate_schema
+from app.helpers.logger import logger_info
 
 
 class FloodZoneSchema(Schema):
     id = fields.Int()
-    cipher = fields.Str()
-    state = fields.Str()
+    cipher = fields.Str(data_key="codigo")
+    state = fields.Str(data_key="estado")
     name = fields.Str(data_key="nombre")
     color = fields.Str()
     coordinates = fields.Nested(
         coordinate_schema, many=True, data_key="coordenadas"
     )
 
+    @post_dump
+    def format_dump(self, data, **kwargs):
+        "Formatea el dump del schema. En especifico el estado de ingles -> español"
+
+        data["estado"] = (
+            "publicado"
+            if data["estado"] == "publicated"
+            else "despublicado"
+        )
+
+        return data
+
 
 flood_zone_schema = FloodZoneSchema(
-    only=("id", "name", "color", "coordinates")
+    only=(
+        "id",
+        "name",
+        "color",
+        "coordinates",
+        "state",
+        "cipher",
+    )
 )
 
 flood_zones_schema = FloodZoneSchema(

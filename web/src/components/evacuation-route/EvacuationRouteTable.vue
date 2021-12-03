ee2cee5243a2f3cee85ec5ccfd2ea7b71994f8ec
Valentin Giorgetti <valengamer13@gmail.com>
2021-12-03 00:38:50 -0300

    Creo el componente "EvacuationRouteTable"
    Se encarga de crear la tabla de recorridos de evacuación

---
+++ web/src/components/evacuation-route/EvacuationRouteTable.vue
@@ -0,0 +1,30 @@
+<template>
+
+    <!-- Tabla de recorridos de evacuación -->
+    <table class="table table-striped">
+        <thead>
+            <tr>
+                <th>{{evacuationRoutes.recorridos.length ? "Nombre" : "No se encontraron recorridos de evacuación"}}</th>
+                <th v-if="evacuationRoutes.recorridos.length">Descripción</th>
+            </tr>
+        </thead>
+
+        <tbody>
+            <tr v-for="(evacuationRoute, index) in evacuationRoutes.recorridos" :key="index">
+                <th scope="row">{{ evacuationRoute.nombre }}</th>  
+                <th scope="row">{{ evacuationRoute.descripcion }}</th>
+            </tr>
+        </tbody>
+    </table>
+
+</template>
+
+<script>
+
+    export default {
+        name: "EvacuationRouteTable",
+
+        props: ["evacuationRoutes"]
+    }
+    
+</script>

385d986afb082d679c33b988862e701cef801094
Valentin Giorgetti <valengamer13@gmail.com>
2021-12-02 23:15:05 -0300

    Creo un componente para dibujar un punto de encuentro en el mapa
    Recibe por parámetro el meeting point que debe dibujar

---
+++ web/src/components/MeetingPoint.vue
@@ -0,0 +1,37 @@
+<template>
+
+    <!-- Se dibuja el punto de encuentro -->
+    <l-marker :lat-lng="[meetingPoint.coordenada.lat, meetingPoint.coordenada.long]">
+
+        <l-icon :icon-url="require('../assets/icons/meeting_point.png')"
+                :icon-size="[30, 40]"/>
+
+        <l-popup :options="{ maxHeight: 350 }">
+            <p style="color:green; text-align:center">Punto de encuentro</p>
+            <strong>Nombre:</strong> {{meetingPoint.nombre}}<br>
+            <strong>Teléfono:</strong> {{meetingPoint.telefono}}<br>
+            <strong>Dirección:</strong> {{meetingPoint.direccion}}<br>
+            <strong>Email:</strong> {{meetingPoint.email}}<br>
+        </l-popup>
+    
+    </l-marker>
+
+</template>
+
+<script>
+
+    import { LPopup, LMarker, LIcon } from "@vue-leaflet/vue-leaflet";
+
+    export default {
+        name: "MeetingPoint",
+
+        components: {
+            LPopup,
+            LMarker,
+            LIcon
+        },
+
+        props: ["meetingPoint"]
+    }
+    
+</script>

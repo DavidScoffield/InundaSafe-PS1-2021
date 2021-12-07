c5ae5b60269b222afca6feababeac623a5afd2d7
Valentin Giorgetti <valengamer13@gmail.com>
2021-12-06 21:28:01 -0300

    Creo el componente "NavigationBar"
    Recibe los items que debe mostrar y la función fetch que debe ejecutar para obtener las páginas

---
+++ web/src/components/NavigationBar.vue
@@ -0,0 +1,36 @@
+<template>
+
+    <!-- Barra de navegación -->
+    <nav>
+        <ul class="pagination justify-content-center">
+            <li v-if="items.pagina > 1" class="page-item">
+                <button class="page-link" tabindex="-1" @click="fetchPage(items.pagina - 1)">Anterior</button>
+            </li>
+            <li v-else class="page-item disabled">
+                <button class="page-link" tabindex="-1">Anterior</button>
+            </li>
+            <li v-for="page in [...Array(items.paginas).keys()]" :key="page" class="page-item"
+                v-bind:class="{ active : items.pagina == page + 1 }">
+                <button class="page-link" @click="fetchPage(page + 1)">{{ page + 1 }}</button>
+            </li>
+            <li v-if="items.pagina < items.paginas" class="page-item">
+                <button class="page-link" @click="fetchPage(items.pagina + 1)">Siguiente</button>
+            </li>
+            <li v-else class="page-item disabled">
+                <button class="page-link">Siguiente</button>
+            </li>
+        </ul>
+    </nav>
+
+</template>
+
+<script>
+
+    export default {
+        name: "NavigationBar",
+
+        props: ["items", 
+                "fetchPage"],
+    }
+    
+</script>

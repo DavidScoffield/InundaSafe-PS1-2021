5cd9893e02746c1dc2e799ce5d13a9c1657c505b
Valentin Giorgetti <valengamer13@gmail.com>
2022-04-03 19:06:02 -0300

    Clase de tests base
    Se define el método tearDown que se ejecuta luego de haber finalizado un test, el cuál elimina la información de la base de datos

---
+++ app/tests/__init__.py
@@ -0,0 +1,10 @@
+import unittest
+from app.db import db
+
+class BaseTestClass(unittest.TestCase):
+
+    def tearDown(self):
+        with self.app.app_context():
+            # Elimina todas las tablas de la base de datos
+            db.session.remove()
+            db.drop_all()
